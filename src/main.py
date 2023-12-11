e032fd3ab6940b65fcdba49d1deb471be22fccdf
Brianaldo <brianaldophandiarta31@gmail.com>
2023-12-12 00:10:04 +0700

    feat: query param middleware parser

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlencode as encode_query_string
+from urllib.parse import parse_qs as parse_query_string
+from starlette.types import ASGIApp, Scope, Receive, Send
 import os
 
 from fastapi import FastAPI
@@ -12,6 +15,32 @@ from fastapi.middleware.cors import CORSMiddleware
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+
+class QueryStringFlatteningMiddleware:
+    def __init__(self, app: ASGIApp) -> None:
+        self.app = app
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        query_string = scope.get("query_string", None).decode()
+        if scope["type"] == "http" and query_string:
+            parsed = parse_query_string(query_string)
+            flattened = {}
+            for name, values in parsed.items():
+                all_values = []
+                for value in values:
+                    all_values.extend(value.split(","))
+
+                flattened[name] = all_values
+
+            # doseq: Turn lists into repeated parameters, which is better for FastAPI
+            scope["query_string"] = encode_query_string(
+                flattened, doseq=True).encode("utf-8")
+
+            await self.app(scope, receive, send)
+        else:
+            await self.app(scope, receive, send)
+
+
 app = FastAPI()
 
 origins = [
@@ -25,6 +54,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(QueryStringFlatteningMiddleware)
+
 
 @app.get("/")
 async def root():
